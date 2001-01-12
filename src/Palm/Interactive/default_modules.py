355aa7e217a9c0f4f8659371a8a3bc993c7cc3fc
jcollins <jcollins>
2001-01-12 19:49:52 +0000

    This module supplies the default modules for the interactive interpreter.

---
+++ src/Palm/Interactive/default_modules.py
@@ -0,0 +1,19 @@
+"""This module contains the default modules for the interactive
+interpreter.  Use the resourcer command to create the module
+resources for the Palm application as follows:
+
+resourcer default_modules
+
+For building a custom application containing your own modules,
+add the following to your top-level module:
+
+from default_modules import *
+
+This implementation is a temporary measure until a Palm
+database has been built to contain the standard Python library
+modules.
+
+"""
+
+import exceptions, types
+
