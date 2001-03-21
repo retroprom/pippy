8d78c4354c91ef54cc63242f00594882e34b8435
jcollins <jcollins>
2001-03-21 14:52:26 +0000

    Refactored the socket module to allow the exportation of the socket API.

--- src/Modules/palm_socketmodule.c
+++ src/Modules/palm_socketmodule.c
@@ -104,57 +104,7 @@ Socket methods:
 #include "set_a4.h"
 #include "_palmutils.h"
 #include "netutil.h"
-
-/*  The following were gleaned from the windows version of the
-    palm SDK:  sdk35-core.zip.  Also, see NetSocket.c. */
-
-/* This makes O_NDELAY the same value for PalmOS as Linux port */
-#ifndef O_NDELAY
-#define O_NDELAY _FNONBLOCK
-#endif
-
-#define FORCE_ANSI_FUNC_DEFS
-
-#ifdef FORCE_ANSI_FUNC_DEFS
-#define BUILD_FUNC_DEF_1( fnname, arg1type, arg1name )	\
-fnname( arg1type arg1name )
-
-#define BUILD_FUNC_DEF_2( fnname, arg1type, arg1name, arg2type, arg2name ) \
-fnname( arg1type arg1name, arg2type arg2name )
-
-#define BUILD_FUNC_DEF_3( fnname, arg1type, arg1name, arg2type, arg2name , arg3type, arg3name )	\
-fnname( arg1type arg1name, arg2type arg2name, arg3type arg3name )
-
-#define BUILD_FUNC_DEF_4( fnname, arg1type, arg1name, arg2type, arg2name , arg3type, arg3name, arg4type, arg4name )	\
-fnname( arg1type arg1name, arg2type arg2name, arg3type arg3name, arg4type arg4name )
-
-#else /* !FORCE_ANSI_FN_DEFS */
-#define BUILD_FUNC_DEF_1( fnname, arg1type, arg1name )	\
-fnname( arg1name )	\
-	arg1type arg1name;
-
-#define BUILD_FUNC_DEF_2( fnname, arg1type, arg1name, arg2type, arg2name ) \
-fnname( arg1name, arg2name )	\
-	arg1type arg1name;	\
-	arg2type arg2name;
-
-#define BUILD_FUNC_DEF_3( fnname, arg1type, arg1name, arg2type, arg2name, arg3type, arg3name ) \
-fnname( arg1name, arg2name, arg3name )	\
-	arg1type arg1name;	\
-	arg2type arg2name;	\
-	arg3type arg3name;
-
-#define BUILD_FUNC_DEF_4( fnname, arg1type, arg1name, arg2type, arg2name, arg3type, arg3name, arg4type, arg4name ) \
-fnname( arg1name, arg2name, arg3name, arg4name )	\
-	arg1type arg1name;	\
-	arg2type arg2name;	\
-	arg3type arg3name;	\
-	arg4type arg4name;
-
-#endif /* !FORCE_ANSI_FN_DEFS */
-
-
-
+#include "palm_socketmodule.h"
 
 
 /* Global variable holding the exception type for errors detected
@@ -189,29 +139,13 @@ palmSocket_Err()
 	return NULL;
 }
 
-/* The object holding a socket.  It holds some extra information,
-   like the address family, which is used to decode socket address
-   arguments properly. */
-
-typedef struct {
-	PyObject_HEAD
-	int sock_fd;		/* Socket file descriptor */
-	int sock_family;	/* Address family, e.g., AF_INET */
-	int sock_type;		/* Socket type, e.g., SOCK_STREAM */
-	int sock_proto;		/* Protocol type, usually 0 */
-	union sock_addr {
-		struct sockaddr_in in;
-
-	} sock_addr;
-} PySocketSockObject;
-
 
 /* A forward reference to the Socktype type object.
    The Socktype variable contains pointers to various functions,
    some of which call newsockobject(), which uses Socktype, so
    there has to be a circular reference. */
 
-staticforward PyTypeObject PySocketSock_Type;
+PyTypeObject PySocketSock_Type;
 
 
 
@@ -1359,7 +1293,7 @@ BUILD_FUNC_DEF_1(PySocketSock_repr,PySocketSockObject *,s)
 
 /* Type object for socket objects. */
 
-static PyTypeObject PySocketSock_Type = {
+PyTypeObject PySocketSock_Type = {
 	PyObject_HEAD_INIT(0)	/* Must fill in type value later */
 	0,
 	"socket",
