4b108f2565accece5079e20aa37c536ab7dd63ed
jricken <jricken>
2001-03-31 01:16:44 +0000

    Cleaned up to consistently use the local buffer.  Made the local buffer a bit bigger.

--- src/Palm/libc/mystdio.c
+++ src/Palm/libc/mystdio.c
@@ -3,12 +3,15 @@
 #include <stdio.h>
 #include "libc_segments.h"
 
-#define BUF_LEN 33
+extern void *malloc(long bytes);
+
+#define BUF_LEN 100
 static char put_buf[BUF_LEN]; /* roughly one line */
 static UInt16 buf_pos = 0;
 
 static int flush_buf() {
 	if (buf_pos > 0) {
+		put_buf[buf_pos]='\0';
 		ioPutS(put_buf);
 		buf_pos = 0;
 	}
@@ -21,33 +24,27 @@ int puts(unsigned char *s) {
 }
 
 int putchar (unsigned int x) {
-	if (buf_pos == 0)
-		MemSet (put_buf, BUF_LEN, '\0');
 	
 	put_buf[buf_pos++] = x;
 
 	/* flush on \n character or when BUF_LEN-1 (keep last \0 for
 	   end-of-string indication) is reached */
 	if (x == '\n' || buf_pos == BUF_LEN-1) {
-		ioPutS(put_buf);
-		buf_pos = 0;
+		flush_buf();
 	}
 
 	return 0;
 }
-static char buf[500];
+
 int printf(const char *fmt, ...)
 {
 	va_list args;
 	int i;
-
+	
 	va_start(args, fmt);
-	i=vsprintf(buf,fmt,args);
+	i = vfprintf(stdout, fmt, args);
 	va_end(args);
-
-	flush_buf();
-	ioPutS(buf);
-
+	
 	return i;
 }
 
@@ -55,25 +52,26 @@ int fprintf(FILE* f, const char *fmt, ...)
 {
 	va_list args;
 	int i;
-
+	
 	va_start(args, fmt);
-	i=vsprintf(buf,fmt,args);
+	i = vfprintf(f, fmt, args);
 	va_end(args);
 	
-	flush_buf();
-	ioPutS(buf);
-
 	return i;
 }
 
 int vfprintf(FILE *stream, const char *fmt, va_list ap)
 {
-	int i;
+	int i,j;
+	char *buf = (char *)malloc(500L);
 
 	i=vsprintf(buf,fmt,ap);
 
-	flush_buf();
-	ioPutS(buf);
+	/*	flush_buf();
+		ioPutS(buf);*/
+	for (j=0; j<i;j++)
+ 	  putchar(buf[j]);
+ 	free(buf);
 
 	return i;
 }
